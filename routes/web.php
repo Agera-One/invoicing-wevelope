95e6650e8453b6806dcac1a9f570b44ec59b6105
Agera-One <182347485+Agera-One@users.noreply.github.com>
2026-09-02 14:12:07 +0700

    separate authenticated and guest routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
@@ -12,9 +13,7 @@ Route::middleware('guest')->group(function () {
 });
 
 Route::middleware('auth')->group(function () {
-    Route::get('/dashboard', function () {
-        return view('welcome');
-    });
+    Route::get('/dashboard', [DashboardController::class, 'index']);
 
     // Route::get('/members', [MemberController::class, 'index']);
     // Route::post('/members', [MemberController::class, 'store'])->name('members.store');
