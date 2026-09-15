3efc6a61960ec5be9104e24aee59ee08468653be
AgeraOne <182347485+Agera-One@users.noreply.github.com>
2026-09-15 22:27:16 +0700

    Refactor HTML structure and styles for invoice PDF

--- app/views/pages/invoice-detail/generate-pdf.php
+++ app/views/pages/invoice-detail/generate-pdf.php
@@ -188,7 +188,7 @@
                 <div style="font-weight: bold; font-size: 14px; margin-bottom: 15px;"><?= $invoice['customer_name'] ?></div>
 
                 <div style="color: #6c757d; font-size: 11px; text-transform: uppercase; margin-bottom: 3px;">Handled by</div>
-                <div style="font-weight: bold;"><?= $invoice['user_name'] ?></div>
+                <div style="font-weight: bold;"><?= $invoice['user_name'] ?></div> 
             </td>
             <td style="width: 50%; text-align: right; vertical-align: top;">
                 <div style="color: #6c757d; font-size: 11px; text-transform: uppercase; margin-bottom: 3px;">Issue date</div>
